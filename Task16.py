f133e18aae67a79b108a226a1b0e0281830ed04d
MamaevEA <mamaevea@gmail.com>
2022-10-06 13:00:36 +0500

    Add Task 16

---
+++ Task16.py
@@ -0,0 +1,18 @@
+#  Вычислить число c заданной точностью d
+# in
+# Enter a real number: 9
+# Enter the required accuracy '0.0001': 0.000001
+
+# out
+# 9.000000
+
+from decimal import Decimal
+
+
+def required_accuracy(count):
+    print("Enter the required accuracy '0.0001':")
+    count = count.quantize(Decimal(input()))
+    print(count)
+
+print ('Enter a real number:')
+required_accuracy(Decimal(input()))
